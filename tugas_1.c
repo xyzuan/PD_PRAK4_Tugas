d8466eb9cbe7a207c5da620a3c0b8325c5b7cde4
xyzuan <xyzuannihboss@gmail.com>
2021-11-18 20:01:13 +0700

    Prak4: Tugas1: Implement scanf function to main looping

--- tugas_1.c
+++ tugas_1.c
@@ -8,29 +8,30 @@
 
 int main(){
 
-    int nilai_pd[2][4] = {{0,85,90,86},{0,84,90,89}};
+    int nilai_pd[2][3];
     int banding;
 
-    // Looping method
     // Nilai Boruto
-    printf("nilai boruto :\n");
-    for (int pd = 1; pd <= 3; pd++){
-        printf("ujian progdas ke-%d : %d\n", pd, nilai_pd[0][pd]);
+    printf("\nnilai boruto : \n");
+    for(int nilai_boruto = 1; nilai_boruto <= 3; nilai_boruto++){
+        printf("ujian progdas ke-%d : ", nilai_boruto);
+        scanf("%d", &nilai_pd[0][nilai_boruto]);
     }
 
     // Nilai Sarada
-    printf("nilai sarada :\n");
-    for (int pd = 1; pd <= 3; pd++){
-        printf("ujian progdas ke-%d : %d\n", pd, nilai_pd[1][pd]);
+    printf("\nnilai sarada : \n");
+    for(int nilai_sarada = 1; nilai_sarada <= 3; nilai_sarada++){
+        printf("ujian progdas ke-%d : ", nilai_sarada);
+        scanf("%d", &nilai_pd[1][nilai_sarada]);
     }
 
     // Perbandingan
     printf("\nrekap perbandingan nilai boruto dan sarada :\n");
     for (int pd = 1; pd <= 3; pd++){ 
-        if(nilai_pd[0][pd] > nilai_pd[1][pd]){
-            printf("hasilnya : 1\n");
-        } else {
+        if(nilai_pd[1][pd] >= nilai_pd[0][pd]){
             printf("hasilnya : 0\n");
+        } else {
+            printf("hasilnya : 1\n");
         }
     }
 
